216ebf2376ed0010038ce7d2c467ed6181300bf2
Supriyo Das <das.supriyo07@gmail.com>
2024-12-20 06:42:59 +0530

    React-TS | Type useState Hook

---
+++ 7_Typing_useState_Hook/my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Count from "./components/Count";
+import Users from "./components/Users";
+
+function App() {
+  return (
+    <>
+      <Count />
+      <br></br>
+      <Users />
+    </>
+  );
+}
+
+export default App;
