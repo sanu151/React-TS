1a08db286f028b24869ce97ae84199cc7d81f287
Supriyo Das <das.supriyo07@gmail.com>
2024-12-22 05:16:32 +0530

    Typing Events

---
+++ 09_Typing_events/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Users from "./components/Users";
+
+function App() {
+  return (
+    <>
+      <h2>Typing Event</h2>
+      <Users />
+    </>
+  );
+}
+
+export default App;
