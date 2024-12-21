1f61cd6d1e60935c0c94db89618889891a6841dd
Supriyo Das <das.supriyo07@gmail.com>
2024-12-22 05:13:32 +0530

    User Defined Prop Union

---
+++ 05_User_defined_prop_types_Union/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Users from "./components/Users";
+
+function App() {
+  return (
+    <>
+      <h1>User Define Prop Types - Union</h1>
+      <Users status="loading" />
+    </>
+  );
+}
+
+export default App;
