bb2984201e7c90cb0e5158fb24f036acec802bd0
Supriyo Das <das.supriyo07@gmail.com>
2024-12-22 05:14:18 +0530

    Typing Children Props

---
+++ 06_Typing_children_props/my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Button from "./components/Button";
+import Posts from "./components/Posts";
+
+function App() {
+  return (
+    <>
+      <h1>Type Children Props</h1>
+      <Posts />
+      <Button>Click Me</Button>
+    </>
+  );
+}
+
+export default App;
